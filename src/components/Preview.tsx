143fd1a98c051f610b0fc51c43eaf5419522b91e
Gandalf69 <adrian992@wp.pl>
2023-11-25 15:38:07 +0100

    Handling async and bundling errors

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,6 +3,7 @@ import './preview.css';
 
 interface PreviewProps {
     code: string;
+    bundlingStatus: string;
 }
 
 const html = `
@@ -13,13 +14,22 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+          const handleError = (err) => {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+          };
+
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error)
+          });
+
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err);
             }
           }, false);
         </script>
@@ -27,7 +37,7 @@ const html = `
     </html>
   `;
 
-const Previerw: React.FC<PreviewProps> = ({code}) => {
+const Previerw: React.FC<PreviewProps> = ({code, bundlingStatus}) => {
     const iframe = useRef<any>();
 
     useEffect(() => {
@@ -45,6 +55,7 @@ const Previerw: React.FC<PreviewProps> = ({code}) => {
           sandbox="allow-scripts"
           srcDoc={html}
         />
+        {bundlingStatus && <div className="preview-error">{bundlingStatus}</div>}
       </div>
         
     );
