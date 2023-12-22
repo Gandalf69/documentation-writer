3e66af71d2028ea3e5c5850ce5611d5e7f5e3939
Gandalf69 <adrian992@wp.pl>
2023-12-22 22:23:46 +0100

    [fix] resize width error

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -10,72 +10,54 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({direction, children}) => {
     const [innerHeight, setInnerHeight] = useState(window.innerHeight); 
     const [innerWidth, setInnerWidth] = useState(window.innerWidth); 
-    const [width, setWidth] = useState(window.innerWidth * 0.75)
+    const [width, setWidth] = useState(Math.floor(window.innerWidth * 0.75));
 
     let resizableProps: ResizableBoxProps;
 
     useEffect(() => {
-        let timer: any;
+		let timer: any;
+		const listener = () => {
+			if (timer) {
+				clearTimeout(timer);
+			}
+ 
+			timer = setTimeout(() => {
+					setInnerHeight(window.innerHeight);
+					setInnerWidth(window.innerWidth);
+					if (window.innerWidth * 0.75 < width) {
+						setWidth(Math.floor(window.innerWidth * 0.75));
+					}
+			}, 100);
+		};
+		window.addEventListener('resize', listener);
+ 
+		return () => {
+			window.removeEventListener('resize', listener);
+		};
+	}, []);
 
-        const listener = () => {
-            if(timer) {
-                clearTimeout(timer)
-            }
-            timer = setTimeout(() => {
-                setInnerWidth(window.innerWidth)
-                setInnerHeight(window.innerHeight)
-                if(window.innerWidth * 0.75 < width) {
-                    setWidth(window.innerWidth * 0.75)
-                }
-            }, 100)
-        }
-
-        window.addEventListener('resize', listener);
-
-        return () => {
-            window.removeEventListener('resize', listener);
-        }
-    }, []);
-
-    useEffect(() => {
-        const resizeErrorListener = (e: ErrorEvent) => {
-        if (e.message ==="ResizeObserver loop completed with undelivered notifications.") {
-                const resizeObserverErrDiv = document.getElementById("webpack-dev-server-client-overlay-div");
-                const resizeObserverErr = document.getElementById("webpack-dev-server-client-overlay");
-                if (resizeObserverErr) {
-                    resizeObserverErr.setAttribute("style", "display: none");
-                }
-                if (resizeObserverErrDiv) {
-                    resizeObserverErrDiv.setAttribute("style", "display: none");
-                }
-                }
-        };
-        window.addEventListener("error", resizeErrorListener);
-    
-        return () => window.removeEventListener("error", resizeErrorListener);
-    }, []);
-
-    if(direction === 'horizontal') {
-        resizableProps = {
-            className: 'resize-horizontal',
-            height: Infinity,
-            width: width,
-            resizeHandles: ['e'],
-            minConstraints: [innerWidth * 0.2, Infinity],
-            maxConstraints: [innerWidth * 0.75, Infinity],
-            onResizeStop: (event, data) => {
-                setWidth(data.size.width)
-            }
-        }
-    } else {
-        resizableProps = {
-            height: 300,
-            width: Infinity,
-            resizeHandles: ['s'],
-            minConstraints: [Infinity, 24],
-            maxConstraints: [Infinity, innerHeight * 0.9]
-        }
-    }
+    if (direction === 'horizontal') {
+		resizableProps = {
+			className: 'resize-horizontal',
+			minConstraints: [Math.floor(innerWidth * 0.2), Infinity],
+			maxConstraints: [Math.floor(innerWidth * 0.75), Infinity],
+			height: Infinity,
+			width,
+			resizeHandles: ['e'],
+			onResizeStop: (event, data) => {
+				setWidth(data.size.width);
+			}
+		};
+	} else {
+		resizableProps = {
+			minConstraints: [Infinity, 24],
+			maxConstraints: [Infinity, Math.floor(innerHeight * 0.9)],
+			height: 300,
+			width: Infinity,
+			resizeHandles: ['s'],
+ 
+		};
+	};
 
     return (
         <ResizableBox 
