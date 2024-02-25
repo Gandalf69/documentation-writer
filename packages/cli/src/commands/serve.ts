21fc8aad4093125ab728a8543edaf44a284186db
Gandalf69 <adrian992@wp.pl>
2024-02-25 15:18:56 +0100

    adding local api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,38 @@
+import path from "path";
 import { Command } from "commander";
+import { serve } from 'local-api';
+
+interface LocalApiError {
+    code: string;
+}
+
+const isLocalApiError = (err: any): err is LocalApiError => {
+    return typeof err.code === "string"
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const serveCommand = new Command()
-    .command('serve')
+    .command('serve [filename]')
     .description('Open a file for editing')
-    .action(() => {
-        console.log('Getting ready to serve a file');
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action(async (filename = 'notebook.js', options: {port: string}) => {
+        try {
+            const dir = path.join(process.cwd(), path.dirname(filename));
+            await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
+            console.log(
+                `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+            );
+        } catch (err) {
+            if(isLocalApiError(err)) {
+                if(err.code === "EADDRINUSE")
+                    console.log("Port is in use. Try running on a different port.");
+                else if(err instanceof Error)
+                    console.log("Here's the problem", err.message);
+                else
+                    console.log("Unknown error", err);
+
+                process.exit(1);
+            }
+        }
     });
